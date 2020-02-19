14b9fa87372bbf1dbe10f72c1f52e20abca2a935
unknown <viola4lfe@gmail.com>
2020-02-19 14:10:20 -0500

    guided solutions

--- data/migrations/20190713153814_create-tables.js
+++ data/migrations/20190713153814_create-tables.js
@@ -1,26 +1,39 @@
 exports.up = function(knex) {
-  return knex.schema
-    .createTable('users', tbl => {
-      tbl.increments();
-      tbl.text('username', 128)
-        .unique()
-        .notNullable();
-    })
-    .createTable('posts', tbl => {
-      tbl.increments();
-      tbl.text('contents');
-      tbl.integer('user_id')
-        .unsigned()
-        .notNullable()
-        .references('id')
-        .inTable('users')
-        .onUpdate('CASCADE')
-        .onDelete('CASCADE');
-    });
+  return (
+    knex.schema
+      // Users Table
+      .createTable("users", tbl => {
+        tbl.increments();
+        tbl
+          .text("username", 128)
+          .unique()
+          .notNullable();
+      })
+      // Posts Table
+      .createTable("posts", tbl => {
+        tbl.increments();
+        tbl.text("contents");
+
+        // foreign key
+        tbl
+          .integer("user_id") // the foreign key must be the same TYPE as the primary key it references.
+
+          .unsigned() // always include  unsigned() when referencing an integer primary key
+
+          .notNullable()
+          .references("id")
+          .inTable("users")
+          // CASCADE
+          .onUpdate("CASCADE") // What happens if the value of the primary key changes? With CASCADE all of the records will update according to the update
+
+          .onDelete("CASCADE"); // what happens if the primary key table row is deleted? (would delete all orders for the primary key)
+        //RESTRICT, DO NOTHING, SET NULL, CASCADE. FOR DELETE RECOMMEND USING RESTRICT NOT CASCADE. This is like having another fail safe before accidentally deleting all your files.
+
+        // if you need it to connect to other tables you would add another column defined in the same way with the matching reference
+      })
+  );
 };
 
 exports.down = function(knex) {
-  return knex.schema
-    .dropTableIfExists('posts')
-    .dropTableIfExists('users');
+  return knex.schema.dropTableIfExists("posts").dropTableIfExists("users");
 };
--- users/user-router.js
+++ users/user-router.js
@@ -1,80 +1,112 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('../data/db-config.js');
+const db = require("../data/db-config.js");
+
+const Users = require("./user-model.js");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  db('users')
-  .then(users => {
-    res.json(users);
-  })
-  .catch (err => {
-    res.status(500).json({ message: 'Failed to get users' });
-  });
+router.get("/", (req, res) => {
+  Users.all()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Failed to get users" });
+    });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   const { id } = req.params;
 
-  db('users').where({ id })
-  .then(users => {
-    const user = users[0];
+  Users.findById(id)
+    .then(users => {
+      const user = users[0];
 
-    if (user) {
-      res.json(user);
-    } else {
-      res.status(404).json({ message: 'Could not find user with given id.' })
-    }
-  })
-  .catch(err => {
-    res.status(500).json({ message: 'Failed to get user' });
-  });
+      if (users) {
+        res.json(users);
+      } else {
+        res.status(404).json({ message: "Could not find user with given id." });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Failed to get user" });
+    });
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   const userData = req.body;
 
-  db('users').insert(userData)
-  .then(ids => {
-    res.status(201).json({ created: ids[0] });
-  })
-  .catch(err => {
-    res.status(500).json({ message: 'Failed to create new user' });
-  });
+  Users.add(userData)
+    .then(ids => {
+      res.status(201).json({ created: ids[0] });
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Failed to create new user" });
+    });
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   const { id } = req.params;
   const changes = req.body;
-
-  db('users').where({ id }).update(changes)
-  .then(count => {
-    if (count) {
-      res.json({ update: count });
-    } else {
-      res.status(404).json({ message: 'Could not find user with given id' });
-    }
-  })
-  .catch(err => {
-    res.status(500).json({ message: 'Failed to update user' });
-  });
+  Users.update(id, changes)
+    .then(count => {
+      if (count) {
+        res.json(changes);
+      } else {
+        res.status(404).json({ message: "Could not find user with given id" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Failed to update user" });
+    });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   const { id } = req.params;
 
-  db('users').where({ id }).del()
-  .then(count => {
-    if (count) {
-      res.json({ removed: count });
-    } else {
-      res.status(404).json({ message: 'Could not find user with given id' });
-    }
-  })
-  .catch(err => {
-    res.status(500).json({ message: 'Failed to delete user' });
-  });
+  Users.remove(id)
+    .then(count => {
+      if (count) {
+        res.json({ removed: count });
+      } else {
+        res.status(404).json({ message: "Could not find user with given id" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Failed to delete user" });
+    });
+});
+
+// list all posts for a user
+
+router.get("/:id/posts", (req, res) => {
+  /* 
+  This is our query
+    select p.contents, u.username as saidBy from posts as p
+    join users as u ON p.contents = u.id
+  */
+
+  db("posts as p")
+    .join("users as u", "p.user_id", "u.id")
+    // syntax = the table you're joining to and the two columns you are joining
+    .select("p.contents", "u.username as saidBy")
+    .where({ user_id: req.params.id })
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(error => {
+      console.log(error, "error");
+      res.status(500).json({ error: "I did it again" });
+    });
 });
 
 module.exports = router;
+
+// function addUser(user) {
+//   return db("users").insert(user, "id");
+// }
+
+// separation of concerns principle = Not wanting everything in one place
+// connected to the "single responsibility principle"
+// a unit should only have one reason to change
